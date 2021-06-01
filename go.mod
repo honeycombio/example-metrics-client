d864f090ceb2415d50f583780131bd0eb56807e3
Max Edmands <max@honeycomb.io>
2021-05-31 19:33:43 -0700

    Instrument app with runtime metrics and traces

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module maxedmands.com/polyhedron
 
 go 1.16
 
-require github.com/justinian/dice v1.0.1
+require (
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/justinian/dice v1.0.1
+	go.opentelemetry.io/contrib/instrumentation/host v0.20.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.20.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.20.0 // indirect
+	go.opentelemetry.io/otel v0.20.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp v0.20.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/justinian/dice"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/contrib/instrumentation/runtime"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/exporters/otlp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
+	metricglobal "go.opentelemetry.io/otel/metric/global"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func roll(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	span := trace.SpanFromContext(ctx)
+
+	nameKey := attribute.Key("name")
+	dieRequestKey := attribute.Key("dieRoll.request")
+	dieResultKey := attribute.Key("dieRoll.result")
+	errorKey := attribute.Key("error")
+
+	ctx = baggage.ContextWithValues(ctx, nameKey.String("roll"))
+
+	span.SetAttributes(nameKey.String("roll"), dieRequestKey.String(req.URL.Path))
+
 	w.Header().Add("Content-Type", "text/html")
 
 	if req.URL.Path == "/" {
@@ -18,13 +44,51 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	result, reason, err := dice.Roll(req.URL.Path)
 	if err != nil {
 		fmt.Fprintf(w, "Error: %v\n", err)
+		span.SetAttributes(errorKey.String(err.Error()))
 		return
 	}
 
+	span.SetAttributes(dieResultKey.Int(result.Int()))
 	fmt.Fprintf(w, "<b>Roll:</b> %s<br /><b>Result:</b> %d<br />%s", result.Description(), result.Int(), reason)
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8090", nil)
+	ctx := context.Background()
+
+	driver := otlpgrpc.NewDriver(
+		otlpgrpc.WithInsecure(),                 // insecure because sending to localhost
+		otlpgrpc.WithEndpoint("localhost:4317"), // otel-collector running as agent on this host (4317 is the default grpc port)
+		otlpgrpc.WithHeaders(map[string]string{"ContentType": "application/grpc"}),
+	)
+
+	exporter, err := otlp.NewExporter(ctx, driver)
+	if err != nil {
+		panic(err)
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(exporter)
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSpanProcessor(bsp))
+	defer func() { _ = tp.Shutdown(ctx) }()
+
+	pusher := controller.New(
+		processor.New(simple.NewWithExactDistribution(), exporter),
+		controller.WithExporter(exporter),
+		controller.WithCollectPeriod(5*time.Second),
+	)
+	metricglobal.SetMeterProvider(pusher.MeterProvider())
+	if err = pusher.Start(ctx); err != nil {
+		panic(err)
+	}
+
+	// Handle this error in a sensible manner where possible
+	defer func() { _ = pusher.Stop(ctx) }()
+
+	if err := runtime.Start(runtime.WithMinimumReadMemStatsInterval(time.Second)); err != nil {
+		panic(err)
+	}
+
+	http.Handle("/", otelhttp.NewHandler(http.HandlerFunc(roll), "Roll"))
+	if err = http.ListenAndServe(":8090", nil); err != nil {
+		panic(err)
+	}
 }
